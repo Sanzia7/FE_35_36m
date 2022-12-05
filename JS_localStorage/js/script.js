346947ab552159942641c40a143a430b1eb8359b
Svetlana <meritee@gmx.de>
2022-12-05 21:18:19 +0100

    js for first level task

---
+++ JS_localStorage/js/script.js
@@ -0,0 +1,40 @@
+function write_text(texts) {
+   localStorage.setItem('texts', JSON.stringify(texts))
+} 
+
+function read_text() {
+   return JSON.parse(localStorage.getItem('texts')) ?? [];
+}
+
+let texts = read_text();
+
+const form = document.querySelector('.add_texts');
+//const texts_container = document.querySelector('.text_areas');
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   const text = event.target.text.value;
+   console.log(text);
+   //texts.push({ id: Date.now(), text })
+   texts.push(text)
+   write_text(texts);
+   //event.target.text.value = '';
+   //rerender()
+});
+
+
+// function rerender() {
+//    //texts_container.innerText = '';
+//    if (read_text().length === 0) {
+//       const p_elem = document.createElement('p')
+//       p_elem.innerText = 'There is no text!'
+//       texts_container.append(p_elem);
+//    } else {
+//       read_text().forEach(item => {
+//          const p_elem = document.createElement('p')
+//          p_elem.innerText = item.text;
+//          texts_container.append(p_elem)
+//       });
+//    }
+// }
+// rerender();
