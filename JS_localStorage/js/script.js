53038d13b908b2d69de15fadb012fb9539120e27
Svetlana <meritee@gmx.de>
2022-12-05 21:37:07 +0100

    js with text in localStorage

--- JS_localStorage/js/script.js
+++ JS_localStorage/js/script.js
@@ -9,32 +9,31 @@ function read_text() {
 let texts = read_text();
 
 const form = document.querySelector('.add_texts');
-//const texts_container = document.querySelector('.text_areas');
+const texts_container = document.querySelector('.text_areas');
 
 form.addEventListener('submit', (event) => {
    event.preventDefault();
    const text = event.target.text.value;
    console.log(text);
-   //texts.push({ id: Date.now(), text })
-   texts.push(text)
+   texts.push({text})
    write_text(texts);
-   //event.target.text.value = '';
-   //rerender()
+   event.target.text.value = '';
+   rerender()
 });
 
 
-// function rerender() {
-//    //texts_container.innerText = '';
-//    if (read_text().length === 0) {
-//       const p_elem = document.createElement('p')
-//       p_elem.innerText = 'There is no text!'
-//       texts_container.append(p_elem);
-//    } else {
-//       read_text().forEach(item => {
-//          const p_elem = document.createElement('p')
-//          p_elem.innerText = item.text;
-//          texts_container.append(p_elem)
-//       });
-//    }
-// }
-// rerender();
+function rerender() {
+   texts_container.innerText = '';
+   if (read_text().length === 0) {
+      const p_elem = document.createElement('p')
+      p_elem.innerText = 'There is no text!'
+      texts_container.append(p_elem);
+   } else {
+      read_text().forEach(item => {
+         const p_elem = document.createElement('p')
+         p_elem.innerText = item.text;
+         texts_container.append(p_elem)
+      });
+   }
+}
+rerender();
