971dfeccf14ed499bedf27ea571d0734127c2250
Svetlana <meritee@gmx.de>
2022-12-06 13:36:29 +0100

    saving external text to internal text

--- JS_Textarea/js/script.js
+++ JS_Textarea/js/script.js
@@ -1,17 +1,23 @@
 const form = document.querySelector('.add_text');
 
-function saved_textarea(text) {
-   localStorage.setItem('text-item', JSON.stringify(text));
-}
+const exterText = document.querySelector('.external_text');
 
+const interText = document.querySelector('#inter_text');
+
+function saved_textarea(interText) {
+   localStorage.setItem('text-item', JSON.stringify(interText));
+}
+saved_textarea();
 function write_textarea() {
    return localStorage.getItem('text-item');
 }
+write_textarea(exterText);
 
 form.addEventListener('submit', (event) => {
    event.preventDefault();
-   const text = event.target.text.value;
-   console.log(text);
-   saved_textarea(text);
-   write_textarea(text);
+   interText.value = exterText.innerText;
+   console.log(interText);
+   console.log(exterText);
+   saved_textarea();
+   write_textarea('text-item');
 })
